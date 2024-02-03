63b9130294158ff02d8e24cbcef65a796d893359
wemsx <3551697397@qq.com>
2024-02-03 21:22:25 +0800

    first update

---
+++ js/title.js
@@ -0,0 +1,8 @@
+var url_string = window.location.href;
+var url = new URL(url_string);
+var url_query = url.searchParams.get('param');
+
+if (url_query != 'null'){
+    var name_string = atob(url_query);
+    document.getElementById("name").innerHTML = name_string + ' ,';
+} else {document.getElementById("name").innerHTML = '';}
