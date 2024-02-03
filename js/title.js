9a433464b60c148bb47961a8fbb4b709eb5db685
wemsx <3551697397@qq.com>
2024-02-03 21:36:56 +0800

    second commit

--- js/title.js
+++ js/title.js
@@ -2,7 +2,9 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var url_query = url.searchParams.get('param');
 
-if (url_query != 'null'){
-    var name_string = atob(url_query);
+if (url_query != 'null') {
+    var name_string = decodeURI(atob(url_query));
     document.getElementById("name").innerHTML = name_string + ' ,';
-} else {document.getElementById("name").innerHTML = '';}
+} else {
+    document.getElementById("name").innerHTML = '';
+}
